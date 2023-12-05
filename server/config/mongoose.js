4a2c706c0d27eeb38af38dcdcdf812f671cb1c5a
jr9648 <jr9648@gmail.com>
2023-12-05 13:30:19 +0530

    ssm-param-added

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,14 +1,36 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+const AWS = require('aws-sdk');
 
-async function main(){
-    try{
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log(`connected to db`);
-    }catch(err){
-        console.log(`error in connecting to db ${err}`);
-    }
+// Configure AWS SDK to use the default credentials provider chain
+AWS.config.update({ region: 'ap-south-1' });
+
+const ssm = new AWS.SSM();
+
+async function getParameterValue(parameterName) {
+  const params = {
+    Name: parameterName,
+    WithDecryption: true // Retrieve the parameter with decryption for SecureString
+  };
+
+  try {
+    const result = await ssm.getParameter(params).promise();
+    return result.Parameter.Value;
+  } catch (err) {
+    throw new Error(`Error fetching parameter ${parameterName} from AWS SSM: ${err.message}`);
+  }
 }
 
+async function main() {
+  try {
+    // Fetch the secure string from AWS SSM
+    const mongoUrl = await getParameterValue('/mongo_url');
+
+    // Connect to MongoDB using the retrieved secure string
+    await mongoose.connect(mongoUrl);
+    console.log(`Connected to db`);
+  } catch (err) {
+    console.log(`Error in connecting to db: ${err}`);
+  }
+}
 
 main();
